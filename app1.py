52fa1ae8fe48ab32837ee3bdac9b0164b7f24e6e
Mike A <amatom128@gmail.com>
2021-02-11 18:23:06 -0500

    from today 21121

--- app1.py
+++ app1.py
@@ -1,16 +1,33 @@
 from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import io
+import csv
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+cors = CORS(app,resource={r"/api/*":{"origins":"*"}})
 
-@app.route('/')
+@app.route('/api')
 def home():
     return render_template("index.html")
 
+@app.route('/api/csv',methods=["POST"])
+def upload():
+    f = request.files['myfile']
+    print(f)
+    if not f:
+        return "No File"
 
+    stream = io.StringIO(f.stream.read().decode("UTF8"),newline=None)
+    csv_input = csv.reader(stream)
+    print(csv_input)
+    for row in csv_input:
+        print(row)
+
+    return render_template("index.html")
     
 if __name__ == "__main__":
     app.run()
