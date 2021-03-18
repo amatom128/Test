1ddfedfa1b8235dbabad3c88efa6085df1963c9f
alexmakdev <alexmakpydev@gmail.com>
2021-03-18 19:52:13 -0400

    implemented logic to write to q/a tables

--- app1.py
+++ app1.py
@@ -1,7 +1,8 @@
 from flask import Flask,render_template,request,redirect, url_for
 from flask_cors import CORS
-from modules.sql import HistoryQuiz
+from modules.sql import HistoryQuestion,HistoryAnswer
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import sessionmaker
 import io
 import csv
 
@@ -15,20 +16,53 @@ cors = CORS(app,resource={r"/api/*":{"origins":"*"}})
 def home():
     return render_template("index.html")
 
+'''
+class HistoryQuiz(Base):
+    __tablename__ = 'historyquiz'
+    quiz_id = Column(Integer,primary_key=True)
+    question = Column(String,nullable=True)
+    answer = Column(String,nullable=True)
+'''
+
 @app.route('/api/csv',methods=["POST"])
 def upload():
-    f = request.files['myfile']
+
+
+    f = request.files['myfile'] 
     print(f)
     if not f:
         return "No File"
 
     stream = io.StringIO(f.stream.read().decode("UTF8"))
     csv_input = csv.reader(stream)
-    print(csv_input)
-    for row in csv_input:
-        print(row)
+    #verify duplicate questions
+    try:
+        for row in csv_input:
+            question = row[0]
+            list_answers = row[1].split(",")
+            #print(list_answers)
+            question = HistoryQuestion(question=row[0])
+            for ele in list_answers:
+                print(ele)
+            #     answer = HistoryAnswer(answer=row[1])
+            db.session.add(question)
+
+
+            db.session.commit()
+
+        db.session.close()
+
+    except Exception as e:
+        print("Error: ",e)
+  
 
     return redirect(url_for('home'))
-    
+
+
+@app.route('/api/play',methods=["GET"])
+def play():
+    pass
+
+
 if __name__ == "__main__":
     app.run()
--- modules/sql.py
+++ modules/sql.py
@@ -1,10 +1,43 @@
-from sqlalchemy import Column,Integer,String
+from sqlalchemy import Column,Integer,String,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
 
 Base = declarative_base()
 
-class HistoryQuiz(Base):
-    __tablename__ = 'historyquiz'
-    quiz_id = Column(Integer,primary_key=True)
-    question = Column(String,nullable=True)
-    answer = Column(String,nullable=True)
+
+class HistoryAnswer(Base):
+    __tablename__ = 'historyanswer'
+    id = Column(Integer,primary_key=True)
+    question_id = Column(Integer,ForeignKey("historyquestion.id"))
+    answer = Column(String)
+    
+    
+
+class HistoryQuestion(Base):
+    __tablename__ = 'historyquestion'
+    id = Column(Integer,primary_key=True)
+    question = Column(String,unique=True)
+    roles = relationship("HistoryAnswer")
+
+
+'''
+create table historyanswer ( 
+      id INTEGER NOT NULL PRIMARY KEY,        
+      question_id INTEGER,
+      answer TEXT,
+      FOREIGN KEY(question_id) REFERENCES historyquestion (id)
+     );   
+
+CREATE TABLE historyquestion ( 
+    id INTEGER NOT NULL PRIMARY KEY,
+    question TEXT
+    );     
+
+'''
+'''
+HistoryAnswer(answer=answer,question=HistoryQuestion.id)
+'''
+
+
+    
